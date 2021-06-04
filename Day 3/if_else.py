a144eee5b27f9d1a3fe6a4a61a3244a3874f7541
Shibra <shibraamin18@gmail.com>
2021-06-04 23:58:41 +0530

    Day 3 completed

---
+++ Day 3/if_else.py
@@ -0,0 +1,25 @@
+# IF-ELSE statement
+# SYNTAX
+# if condition:
+#   do this
+# else:
+#  do this
+
+# Comparison Operators used in IF-ELSE
+
+# > - Greater than
+# > - less than
+# >= - Greater than or equal to
+# <= - less than or equal to
+# == - equal to
+# != - NOT equal to
+
+#  1
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("You can ride the rollercoaster")
+else:
+  print("Sorry! you have grow-up taller to ride the rollercoaster\n")
+
